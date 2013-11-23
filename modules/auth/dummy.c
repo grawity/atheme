e7ca146d16caa9433854bd87a5bee2de61536d48
Mantas Mikulėnas <grawity@gmail.com>
2013-11-23 23:25:38 +0200

    initial

--- modules/auth/dummy.c
+++ modules/auth/dummy.c
@@ -8,30 +8,82 @@
 
 #include "atheme.h"
 
+#include <curl/curl.h>
+
 DECLARE_MODULE_V1
 (
-	"auth/dummy", false, _modinit, _moddeinit,
+	"auth/http", false, _modinit, _moddeinit,
 	PACKAGE_STRING,
 	"Atheme Development Group <http://www.atheme.org>"
 );
 
+struct authdata {
+	myuser_t *mu,
+	const char *pw
+};
+
+static size_t authpostdata(void *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	struct authdata *state = userdata;
+
+
+}
+
 static bool dummy_auth_user(myuser_t *mu, const char *password)
 {
-	return false;
+	CURL *ch = NULL;
+	bool ok = false;
+	struct authdata *state = NULL;
+
+	state = malloc(sizeof *state);
+	if (!state)
+		goto fin;
+
+	*state = (struct authdata) { .mu = mu, .pw = password };
+
+	ch = curl_easy_init();
+	if (!ch) goto fin;
+	
+	cr = curl_easy_setopt(ch, CURLOPT_VERBOSE, 1);
+	if (cr) goto fin;
+
+	cr = curl_easy_setopt(ch, CURLOPT_URL, "http://localhost/foo");
+	if (cr) goto fin;
+
+	cr = curl_easy_setopt(ch, CURLOPT_POST, 1);
+	if (cr) goto fin;
+
+	cr = curl_easy_setopt(ch, CURLOPT_TIMEOUT, 5);
+	if (cr) goto fin;
+
+	cr = curl_easy_setopt(ch, CURLOPT_READFUNCTION, authpostdata);
+
+	cr = curl_easy_setopt(ch, CURLOPT_READDATA, state);
+	if (cr) goto fin;
+
+	cr = curl_easy_perform(ch);
+	if (cr) goto fin;
+
+fin:
+	if (state) free(state);
+	if (ch) curl_easy_cleanup(ch);
+	return ok;
 }
 
 void _modinit(module_t *m)
 {
-	auth_user_custom = &dummy_auth_user;
-
-	auth_module_loaded = true;
+	if (curl_global_init(CURL_GLOBAL_ALL) == 0) {
+		auth_user_custom = &dummy_auth_user;
+		auth_module_loaded = true;
+	}
 }
 
 void _moddeinit(module_unload_intent_t intent)
 {
 	auth_user_custom = NULL;
-
 	auth_module_loaded = false;
+
+	curl_global_cleanup();
 }
 
 /* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
